a738d68cf35fa01a582c988e57eb1f2cad40cab8
Anuj Shrestha <livanuj@gmail.com>
2023-04-07 17:22:25 +0000

    added test and actions for POST, PATCH and delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  username = Faker::Internet.user('username')[:username]
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  User.create(username: username, name: name, email: email )
+end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -18,4 +18,116 @@ RSpec.describe "Users API", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  # Test suite for GET /users/:id
+  describe 'GET /users/:id' do
+    before { get "/users/#{user_id}" }
+
+    context 'when the user exists' do
+      it 'returns the user' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(user_id)
+        expect(json['username']).to eq(User.find(user_id).username)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the user does not exist' do
+      let(:user_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match("{\"message\":\"Couldn't find User with 'id'=100\"}")
+      end
+    end
+  end
+
+  # Test suite for POST /users
+  describe 'POST /users' do
+    # valid payload
+    let(:valid_attributes) {{
+      name: 'Anuj Shrestha',
+      username: 'anuj8',
+      email: 'abc@example.com',
+      website: 'http://faketest.com'
+    }}
+
+    context 'when the request is valid' do
+      before { post '/users', params: valid_attributes }
+
+      it 'creates a user' do
+        expect(json['name']).to eq('Anuj Shrestha')
+        expect(json['username']).to eq('anuj8')
+        expect(json['email']).to eq('abc@example.com')
+        expect(json['website']).to eq('http://faketest.com')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/users', params: { email: 'anuj' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match("{\"message\":\"Validation failed: Username can't be blank, Email is invalid\"}")
+      end
+    end
+  end
+
+  # Test suite for PUT /users/:id
+  describe 'PUT /users/:id' do
+    let(:valid_attributes) { { email: 'anuj@gmail.com' } }
+    let(:invalid_attributes) { { email: 'anuj...' } }
+
+    context 'when the record exists' do
+      before { put "/users/#{user_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).not_to be_empty
+        expect(json['email']).to eq('anuj@gmail.com')
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'updates invalid record' do
+      before { put "/users/#{user_id}", params: invalid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to match("{\"message\":\"Validation failed: Email is invalid\"}")
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+   # Test suite for DELETE /users/:id
+   describe 'DELETE /users/:id' do
+    before { delete "/users/#{user_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'check if record got deleted' do
+      expect { User.find(user_id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
